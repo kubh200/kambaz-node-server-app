390ed917fda041f005361a550b2f81d2d01c2813
Duxbeh <zijiechen122@gmail.com>
2025-04-14 22:44:50 -0400

    aaaassss

--- Kambaz/Assignments/dao.js
+++ Kambaz/Assignments/dao.js
@@ -1,27 +1,53 @@
-import Database from "../Database/index.js";
+// import Database from "../Database/index.js";
 import { v4 as uuidv4 } from "uuid";
+import AssignmentModel from "./model.js";
 
-export const findAssignmentsForCourse = (courseId) => {
-  return Database.assignments.filter((a) => a.course === courseId);
+// 1. Find all assignments for a course
+export const findAssignmentsForCourse = async (courseId) => {
+  return AssignmentModel.find({ course: courseId }).populate("course");
 };
 
-export const createAssignment = (courseId, assignment) => {
+// 2. Create a new assignment in a course
+export const createAssignment = async (courseId, assignment) => {
   const newAssignment = {
     ...assignment,
-    _id: uuidv4(),
+    _id: uuidv4(), // or use Mongo's default ObjectId if preferred
     course: courseId,
   };
-  Database.assignments.push(newAssignment);
-  return newAssignment;
+  return AssignmentModel.create(newAssignment);
 };
 
-export const updateAssignment = (assignmentId, updates) => {
-  const assignment = Database.assignments.find((a) => a._id === assignmentId);
-  if (!assignment) return null;
-  Object.assign(assignment, updates);
-  return assignment;
+// 3. Update an assignment by ID
+export const updateAssignment = async (assignmentId, updates) => {
+  await AssignmentModel.updateOne({ _id: assignmentId }, { $set: updates });
+  return AssignmentModel.findById(assignmentId).populate("course"); // return the updated document
 };
 
-export const deleteAssignment = (assignmentId) => {
-  Database.assignments = Database.assignments.filter((a) => a._id !== assignmentId);
+// 4. Delete an assignment by ID
+export const deleteAssignment = async (assignmentId) => {
+  return AssignmentModel.deleteOne({ _id: assignmentId });
 };
+// export const findAssignmentsForCourse = (courseId) => {
+//   return Database.assignments.filter((a) => a.course === courseId);
+// };
+
+// export const createAssignment = (courseId, assignment) => {
+//   const newAssignment = {
+//     ...assignment,
+//     _id: uuidv4(),
+//     course: courseId,
+//   };
+//   Database.assignments.push(newAssignment);
+//   return newAssignment;
+// };
+
+// export const updateAssignment = (assignmentId, updates) => {
+//   const assignment = Database.assignments.find((a) => a._id === assignmentId);
+//   if (!assignment) return null;
+//   Object.assign(assignment, updates);
+//   return assignment;
+// };
+
+// export const deleteAssignment = (assignmentId) => {
+//   Database.assignments = Database.assignments.filter((a) => a._id !== assignmentId);
+// };
