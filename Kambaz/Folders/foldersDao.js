11868986f2074760eb36911590dfd15b9d4e37a3
Duxbeh <zijiechen122@gmail.com>
2025-04-21 01:48:23 -0400

    sever pazza update 2

--- Kambaz/Folders/foldersDao.js
+++ Kambaz/Folders/foldersDao.js
@@ -1,10 +1,13 @@
 import foldersModel from "./foldersModel.js";
+import { v4 as uuidv4 } from "uuid";
 
 export const findFoldersByCourse = async (courseId) =>
   await foldersModel.find({ course: courseId });
 
-export const createFolder = async (folder) =>
-  await foldersModel.create(folder);
+export const createFolder = async (folder) => {
+  const folderWithId = { ...folder, _id: uuidv4() }; // 🆕 Add _id
+  return await foldersModel.create(folderWithId);
+};
 
 export const updateFolder = async (folderId, folder) =>
   await foldersModel.updateOne({ _id: folderId }, { $set: folder });
--- Kambaz/Folders/foldersSchema.js
+++ Kambaz/Folders/foldersSchema.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const foldersSchema = new mongoose.Schema({
-  _id: String,             // e.g., "hw1", "logistics"
+  _id: String,             // may use uuid
   name: String,            // e.g., "HW1", "Logistics"
   course: String           // courseId like "RS101"
 }, { collection: "folders" });
